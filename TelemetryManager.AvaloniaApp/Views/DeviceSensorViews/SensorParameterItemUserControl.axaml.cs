95bd5d48f1fe8c247cab8e101e2af0352752be74
volodya.dichkovski <volodya.dichkovski@gmail.com>
2025-07-03 14:14:34 +0300

    /add SensorParameterUserControl

---
+++ TelemetryManager.AvaloniaApp/Views/DeviceSensorViews/SensorParameterItemUserControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TelemetryManager.AvaloniaApp.Views.DeviceSensorViews;
+
+public partial class SensorParameterItemUserControl : UserControl
+{
+    public SensorParameterItemUserControl()
+    {
+        InitializeComponent();
+    }
+}
